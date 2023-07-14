dba8eb59f707dc8014ce18c8514b28e3aa5bfabb
ILDUS <ildus6696@gmail.com>
2023-07-14 22:24:22 +0300

    Zadacha reshena

--- Program.cs
+++ Program.cs
@@ -5,8 +5,19 @@ String[] array = new string[n];
 int count = 0;
 for (int i = 0; i < array.Length; i++)
 {
-    Console.Write($"Введите значение {i+1}:");
+    Console.Write($"Введите значение {i+1}: ");
     array[i] = Console.ReadLine();
     if (array[i].Length <= 3) count++;
 }
 Console.WriteLine($"Введенный массив: [{string.Join(", ", array)}]");
+String[] array1 = new string[count];
+int j = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        array1[j] = array[i];
+        j++;
+    }
+}
+Console.WriteLine($"Новый массив: [{string.Join(", ", array1)}]");
