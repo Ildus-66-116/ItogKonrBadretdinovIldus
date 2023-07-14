973deb6016093897109355eec6c20bc25c484a89
ILDUS <ildus6696@gmail.com>
2023-07-14 21:49:04 +0300

    Vvod massiva

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = int.Parse(Console.ReadLine()!);
+String[] array = new string[n];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Введите значение {i+1}:");
+    array[i] = Console.ReadLine();
+}
+Console.WriteLine($"Введенный массив: [{string.Join(", ", array)}]");
