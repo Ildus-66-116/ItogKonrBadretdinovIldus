36bb69a0166dcdd5ec97375fb1a75484ed2509cd
ILDUS <ildus6696@gmail.com>
2023-07-14 21:53:49 +0300

    Vveden kol elementov novogo massiva

--- Program.cs
+++ Program.cs
@@ -2,9 +2,11 @@
 Console.Write("Введите кол-во элементов массива: ");
 int n = int.Parse(Console.ReadLine()!);
 String[] array = new string[n];
+int count = 0;
 for (int i = 0; i < array.Length; i++)
 {
     Console.Write($"Введите значение {i+1}:");
     array[i] = Console.ReadLine();
+    if (array[i].Length <= 3) count++;
 }
 Console.WriteLine($"Введенный массив: [{string.Join(", ", array)}]");
